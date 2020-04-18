2f7a279fd0fec5bb6cc8830790b370e91c7fa6b3
rmocorrea <bjj.rocorrea@gmail.com>
2020-04-18 14:20:43 -0300

    Atual

--- Exercicio118/Program.cs
+++ Exercicio118/Program.cs
@@ -43,7 +43,7 @@ namespace Exercicio118
             int year = int.Parse(monthAndYear.Substring(3));
             Console.WriteLine("Name: " + worker.Name);
             Console.WriteLine("Department: " + worker.Department.Name);
-            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
