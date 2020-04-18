626695bf417d48b6d08fb991adc3f8dca5cf219c
rmocorrea <bjj.rocorrea@gmail.com>
2020-04-18 14:10:03 -0300

    Worker teste em aula

---
+++ Exercicio118/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio118.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Exercicio118/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio118.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
